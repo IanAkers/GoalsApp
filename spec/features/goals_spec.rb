53e7ee1a9fd931ac063862cc423894a7405c11c1
Ian&Tom <Ian&Tom@appacademy.io>
2015-06-22 12:31:23 -0400

    Add more goals specs.

--- spec/features/goals_spec.rb
+++ spec/features/goals_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 
 before(:each) do
   sign_up("testname")
-  sign_in("testname")
 end
 
 feature "user can create a goal" do
@@ -27,3 +26,66 @@ feature "user can create a goal" do
 end
 
 feature "has goal privacy" do
+  scenario "doesn't show private goals" do
+    create_goal("Brush teeth once a week", "Private")
+    sign_out
+    sign_up("othername")
+    visit("/goals")
+    expect(page).not_to have_content("Brush teeth once a week")
+  end
+
+  scenario "does show public goals" do
+    create_goal("Give to charity", "Public")
+    sign_out
+    sign_up("othername")
+    visit("/goals")
+    expect(page).to have_content("Give to charity")
+  end
+end
+
+feature "shows username on goals index" do
+  scenario "shows name when public goal is posted" do
+    create_goal("Wear more hats", "Public")
+    sign_out
+    sign_up("othername")
+    visit("/goals")
+    expect(page).to have_content("testname")
+  end
+end
+
+feature "user can destroy goals" do
+  scenario "user can destroy own goals" do
+    create_goal("Wear less hats", "Public")
+    click_link('My Profile')
+    click_button("destroy_Wear_less_hats")
+    expect(page).not_to have_content("Wear less hats")
+  end
+
+  scenario "user can't destroy other users' goals" do
+    create_goal("Learn to juggle", "Public")
+    sign_out
+    sign_up("othername")
+    visit("/goals")
+    click_link "testname"
+    expect(page).not_to have_button("destroy_Learn_to_juggle")
+  end
+end
+
+feature "user can update goals" do
+  before(:each) do
+    create_goal("Edit more goals", "Public")
+    click_link("My Profile")
+    click_button("edit_Edit_more_goals")
+  end
+
+  scenario "edit page exists" do
+    expect(page).to have_content("Edit Goal")
+  end
+
+  scenario "user can change goal content" do
+    fill_in
+  end
+
+
+
+end
