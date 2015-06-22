3a0f27f241d8e16af7842b3de03afb1256c3e6ae
Ian&Tom <Ian&Tom@appacademy.io>
2015-06-22 10:10:21 -0400

    Initialize GoalsApp rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoalApp::Application.config.secret_key_base = 'aaf9a9673f48cf4a1c46119074d0cdbcb55a90839960fe32865d20c0f86f6b8f8ebafcc5d9c0924a6d1565388423f187cdeb75a0125b59cf71f9b2d78f3eedca'
