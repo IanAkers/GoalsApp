48fbd3bfb0e812b77fca23a3a0c31cc60f22bf36
Ian&Tom <Ian&Tom@appacademy.io>
2015-06-22 14:42:30 -0400

    Finish goals_controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,4 +3,45 @@ class GoalsController < ApplicationController
   def index
     render :index
   end
+
+  def new
+    @goal = current_user.goals.new
+  end
+
+  def create
+    @goal = current_user.goals.new(goal_params)
+    if @goal.save
+      redirect_to goals_url
+    else
+      flash[:errors] = @goal.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+
+  def update
+    @goal = Goal.find(params[:id])
+    if @goal.update(goal_params)
+      flash[:notices] = ["Goal Updated"]
+      redirect_to goals_url
+    else
+      flash[:errors] = @goal.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @goal = Goal.find(params[:id])
+    @goal.destroy
+    redirect_to goals_url
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:content, :goal_type, :goal_status)
+  end
 end
