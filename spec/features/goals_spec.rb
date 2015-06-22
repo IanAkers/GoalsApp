30d8cf35744bc917832a9bb3fd9cc2ca8455bc6b
Ian&Tom <Ian&Tom@appacademy.io>
2015-06-22 13:58:56 -0400

    Complete initial goals spec file.

--- spec/features/goals_spec.rb
+++ spec/features/goals_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 require 'rails_helper'
 
-before(:each) do
-  sign_up("testname")
-end
+
 
 feature "user can create a goal" do
+
+  before(:each) do
+    sign_up("testname")
+  end
 
   scenario "shows create goal button on homepage" do
     visit("/goals")
@@ -26,6 +28,11 @@ feature "user can create a goal" do
 end
 
 feature "has goal privacy" do
+
+  before(:each) do
+    sign_up("testname")
+  end
+
   scenario "doesn't show private goals" do
     create_goal("Brush teeth once a week", "Private")
     sign_out
@@ -44,6 +51,11 @@ feature "has goal privacy" do
 end
 
 feature "shows username on goals index" do
+
+  before(:each) do
+    sign_up("testname")
+  end
+
   scenario "shows name when public goal is posted" do
     create_goal("Wear more hats", "Public")
     sign_out
@@ -54,6 +66,11 @@ feature "shows username on goals index" do
 end
 
 feature "user can destroy goals" do
+
+  before(:each) do
+    sign_up("testname")
+  end
+
   scenario "user can destroy own goals" do
     create_goal("Wear less hats", "Public")
     click_link('My Profile')
@@ -72,7 +89,9 @@ feature "user can destroy goals" do
 end
 
 feature "user can update goals" do
+
   before(:each) do
+    sign_up("testname")
     create_goal("Edit more goals", "Public")
     click_link("My Profile")
     click_button("edit_Edit_more_goals")
@@ -83,9 +102,28 @@ feature "user can update goals" do
   end
 
   scenario "user can change goal content" do
-    fill_in
+    fill_in("Content", with: "Learn Ruby")
+    click_button("Update Goal")
+    expect(page).to have_content("Learn Ruby")
   end
 
+  scenario "old goal changes when goal is updated" do
+    fill_in("Content", with: "Learn Ruby")
+    click_button("Update Goal")
+    expect(page).not_to have_content("Edit more goals")
+  end
 
+  scenario "user can change goal privacy type" do
+    choose("Private")
+    click_button("Update Goal")
+    sign_out
+    sign_up("othername")
+    expect(page).not_to have_content("Edit more goals")
+  end
 
+  scenario "user can change goal status" do
+    choose("Completed")
+    click_button("Update Goal")
+    expect(page).to have_content("Completed")
+  end
 end
